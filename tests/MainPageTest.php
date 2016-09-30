5ffd899b1831cd458f8944d2fa947e645a3f0fb6
Ronald Marangwanda <ronald.marangwanda@gmail.com>
2016-09-29 20:11:54 -0400

    renamed test file

--- tests/MainPageTest.php
+++ tests/MainPageTest.php
@@ -4,16 +4,15 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class MainPageTest extends TestCase
 {
     /**
-     * A basic functional test example.
-     *
+     *      *
      * @return void
      */
     public function testBasicExample()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+             ->see('Mp3AfricaMusic.com');
     }
 }
